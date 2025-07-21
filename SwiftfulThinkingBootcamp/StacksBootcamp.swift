578830a88a167f175747943d8b92b6d264b41b68
DavidPMay <maydavidp@icloud.com>
2025-07-20 20:06:48 -0400

    [Feature] VStack demo

---
+++ SwiftfulThinkingBootcamp/StacksBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  StacksBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by David May on 7/20/25.
+//
+
+import SwiftUI
+
+struct StacksBootcamp: View {
+    // Vstack -> Vertical
+    // Hstack -> Horizontal
+    // Zstack -> Zindex (back to front)
+    
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 100, height: 100)
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 100, height: 100)
+            
+            Rectangle()
+                .fill(Color.orange)
+                .frame(width: 100, height: 100)
+                
+                
+        }
+    }
+}
+
+#Preview {
+    StacksBootcamp()
+}
