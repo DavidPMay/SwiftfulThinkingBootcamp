d612f405da8a696a471e9bfc5123b335351952e9
DavidPMay <maydavidp@icloud.com>
2025-07-20 20:10:46 -0400

    [Feature] HStack Demo

--- SwiftfulThinkingBootcamp/StacksBootcamp.swift
+++ SwiftfulThinkingBootcamp/StacksBootcamp.swift
@@ -13,7 +13,23 @@ struct StacksBootcamp: View {
     // Zstack -> Zindex (back to front)
     
     var body: some View {
-        VStack {
+//        VStack {
+//            Rectangle()
+//                .fill(Color.red)
+//                .frame(width: 100, height: 100)
+//            
+//            Rectangle()
+//                .fill(Color.green)
+//                .frame(width: 100, height: 100)
+//            
+//            Rectangle()
+//                .fill(Color.orange)
+//                .frame(width: 100, height: 100)
+//                
+//                
+//        }
+        
+        HStack {
             Rectangle()
                 .fill(Color.red)
                 .frame(width: 100, height: 100)
