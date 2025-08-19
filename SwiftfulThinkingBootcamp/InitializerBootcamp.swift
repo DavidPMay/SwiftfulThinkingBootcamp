41331fd32066b4f40743f4d14a553a5627893013
DavidPMay <maydavidp@icloud.com>
2025-08-19 18:40:22 -0400

    [Feature] Finished coding bootcamp for initializer and enum

--- SwiftfulThinkingBootcamp/InitializerBootcamp.swift
+++ SwiftfulThinkingBootcamp/InitializerBootcamp.swift
@@ -8,22 +8,63 @@
 import SwiftUI
 
 struct InitializerBootcamp: View {
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    
+    init( count: Int, fruit: Fruit) {
+        
+        self.count = count
+        
+        
+        if fruit == .bananas {
+                   self.backgroundColor = .yellow
+                self.title = "Bananas"
+        } else if fruit == .apples {
+                   self.backgroundColor = .red
+                   self.title = "Apples"
+               } else {
+                   self.backgroundColor = .orange
+                   self.title = "Oranges"
+               }
+           }
+//        if title == "Bananas" {
+//            self.backgroundColor = .yellow
+//        } else if title == "Apples" {
+//            self.backgroundColor = .red
+//        } else {
+//            backgroundColor = .orange
+//        }
+//    }
+    
+    enum Fruit {
+        case bananas
+        case apples
+        case oranges
+    }
+    
     var body: some View {
+        
         VStack(spacing: 12) {
-            Text("5")
+            Text("\(count)")
                 .font(.largeTitle)
                 .foregroundColor(.white)
                 .underline()
-            Text("Apples")
+            Text(title)
                 .font(.headline)
                 .foregroundColor(.white)
         }
-        .frame(width: 150, height: 150)
-        .background(Color.red)
+        .frame(width: 120, height: 120)
+        .background(backgroundColor)
         .cornerRadius(10)
     }
 }
 
 #Preview {
-    InitializerBootcamp()
+    HStack {
+        InitializerBootcamp(count: 55, fruit: .apples)
+        InitializerBootcamp(count: 46, fruit: .bananas)
+        InitializerBootcamp(count: 27, fruit: .oranges)
+    }
 }
